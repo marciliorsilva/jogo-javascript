3277ad51a7c4c6c918421922b379a20da51ab9d3
Marcilio <marciliorsilva93@gmail.com>
2020-11-20 10:29:52 -0300

    criando cobrinha

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
 let canvas = document.getElementById("snake");
 let contex = canvas.getContext("2d");
 let box = 32;
-
+let snake = [];
+snake[0] = {
+    x: 8 * box,
+    y: 8 * box
+}
 function criarBG(){
     contex.fillStyle = "lightgreen";
     contex.fillRect(0, 0, 16 * box, 16 *box);
 
 }
 
+function criarCobrinha(){
+  for (let index = 0; index < snake.length; index++) {
+      contex.fillStyle = "green";
+      contex.fillRect(snake[index].x, snake[index].y, box, box)
+      
+  }
+}
 criarBG();
+criarCobrinha();
